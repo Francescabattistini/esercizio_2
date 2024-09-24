b621476af6d1e0fff6151b6ec278783c4bcc578a
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-24 16:39:09 +0200

    ok

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("esercizio 1");
@@ -5,12 +7,56 @@ public class Main {
          String test2= "amicoo";
          System.out.println(stringPariDispari( test1));
          System.out.println(stringPariDispari(test2));
-        System.out.println("esercizio 2");
-        
-    }
+        System.out.println("esercizio 1.2");
+        int anno1 = 2020;
+        int anno2 = 1900;
+        int anno3 = 2025;
 
+        System.out.println(annoBisestile(anno1));
+        System.out.println(annoBisestile(anno2));
+        System.out.println(annoBisestile(anno3));
+        System.out.println("esercizio 2");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci un numero intero tra 0 e 3: ");
+
+        int numero = scanner.nextInt();
+
+        switch (numero) {
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("Uno");
+                break;
+            case 2:
+                System.out.println("Due");
+                break;
+            case 3:
+                System.out.println("Tre");
+                break;
+            default:
+
+                System.out.println("Errore: numero fuori dall'intervallo");
+        }
+        scanner.close();
+
+
+    }
+//esercizio 1
     public static boolean stringPariDispari(String str){
         return str.length()%2==0;
+    }
+    //esercizio 2
+    public static boolean annoBisestile(int  anno){
+        if (anno% 4==0){
+            if(anno % 100==0){
+                return anno % 400 == 0;
+            }
+            return true;
+        }
+        return false;
     }
 }
 
