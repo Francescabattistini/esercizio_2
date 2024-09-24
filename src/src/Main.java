135cfef8e4f269e732c986bb9691141a91638f15
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-24 15:42:24 +0200

    4

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("esercizio 1");
          String test1 = "ciao";
          String test2= "amicoo";
          System.out.println(stringPariDispari( test1));
          System.out.println(stringPariDispari(test2));
+        System.out.println("esercizio 2");
+        
     }
 
     public static boolean stringPariDispari(String str){
         return str.length()%2==0;
     }
 }
+
